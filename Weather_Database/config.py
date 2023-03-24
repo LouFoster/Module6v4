a65e7a9b7ddb82a60456b5cfa465259683fb289c
Lou Foster <lawuana.foster@att.net>
2023-03-23 19:52:37 -0500

    updates

--- Weather_Database/config.py
+++ Weather_Database/config.py
@@ -1,1 +1,3 @@
 weather_api_key="dd4489015a0d53691a4504c482fe26b5"
+# Geoapify API Key
+geoapify_key = "YOU KEY GOES HERE!"
--- World_Weather_Analysis/config.py
+++ World_Weather_Analysis/config.py
@@ -1,1 +1,3 @@
 weather_api_key="dd4489015a0d53691a4504c482fe26b5"
+# Geoapify API Key
+geoapify_key = "114208dbe31c49a6aee5748cfdc3a2e5"
